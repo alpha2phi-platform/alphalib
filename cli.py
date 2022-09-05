6713df24dbc34d64d129c8cb8b7634b3c5342604
alpha2phi <alpha2phi@gmail.com>
2022-09-05 09:59:28 +0800

    feat: downloading dataset

--- cli.py
+++ cli.py
@@ -6,7 +6,7 @@ from alphalib.dataset import Dataset
 class AlphaLib(object):
     def download(self, country="united states"):
         dataset = Dataset(country=country)
-        dataset.download(continue_from_last_download=True, start_pos=2200)
+        dataset.download(continue_from_last_download=True, start_pos=1905)
 
 
 if __name__ == "__main__":
