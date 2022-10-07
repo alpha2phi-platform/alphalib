08cd950a5e3dbde2195836dad636077b62381ec9
alpha2phi <alpha2phi@gmail.com>
2022-10-07 17:57:07 +0800

    feat: earnings and dividends

--- alphalib/data_sources/nasdaq.py
+++ alphalib/data_sources/nasdaq.py
@@ -3,14 +3,12 @@ from datetime import datetime
 
 import pandas as pd
 import requests
-from lxml.cssselect import CSSSelector
-from lxml.html import fromstring
+from bs4 import BeautifulSoup
+from bs4.element import Tag
 from requests.adapters import HTTPAdapter
 
 from ..utils.httputils import (DEFAULT_HTTP_RETRY, DEFAULT_HTTP_TIMEOUT,
                                http_headers)
-
-name_selector = CSSSelector("div.dividend-history__heading > h1")
 
 
 @dataclass
@@ -25,30 +23,35 @@ class Nasdaq:
 
 def get_dividend_history(url: str) -> Nasdaq:
     assert url is not None
-    
+
     nasdaq = Nasdaq()
-
     s = requests.Session()
-    s.mount("https://", HTTPAdapter(max_retries=DEFAULT_HTTP_RETRY))
-    r = s.get(url, verify=True,headers=http_headers(), timeout=DEFAULT_HTTP_TIMEOUT)
-    if r.status_code != 200:
-        raise ConnectionError(
-            "ERR: error " + str(r.status_code) + ", try again later."
+    try:
+        s.verify = False
+        s.mount("https://", HTTPAdapter(max_retries=DEFAULT_HTTP_RETRY))
+        r = s.get(
+            url, verify=True, headers=http_headers(), timeout=DEFAULT_HTTP_TIMEOUT
         )
-    h = fromstring(r.text)
+        if r.status_code != 200:
+            raise ConnectionError(
+                "ERR: error " + str(r.status_code) + ", try again later."
+            )
+        soup = BeautifulSoup(
+            s.get(
+                url, verify=True, headers=http_headers(), timeout=DEFAULT_HTTP_TIMEOUT
+            ).text,
+            "lxml",
+        )
 
-    # Get name
-    elems = name_selector(h)
-    if elems:
-        nasdaq.label = elems[0].text  # type: ignore
+        # Get the stock label
+        tag: Tag | None = soup.select_one("div.dividend-history__heading > h1")
+        if tag:
+            nasdaq.label = tag.text
 
-    # elem = res.html.find("div.dividend-history__heading > h1", first=True)
-    # if elem:
 
-    # # Ex dividend date
-    # elem = res.html.find(
-    #         "ul > li:nth-child(1) > span.dividend-history__summary-item__value" , first=True,
-    # )
-    # print(elem)  # type: ignore
+        # Get ex dividend date
+
+    finally:
+        s.close()
 
     return nasdaq
