6f1cf7c295ce1a881ab3f0c524474ebf3be4e39f
alpha2phi <alpha2phi@gmail.com>
2022-10-18 17:34:24 +0800

    feat: high-yield stocks

--- alphalib/dataset/high_yield.py
+++ alphalib/dataset/high_yield.py
@@ -1,10 +1,13 @@
+from contextlib import closing
 from dataclasses import dataclass
 
+import requests
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
+from requests.adapters import HTTPAdapter
 
 from alphalib.utils.convertutils import TypeConverter, strip, to_float
-from alphalib.utils.httputils import web_driver
+from alphalib.utils.httputils import (DEFAULT_HTTP_RETRY, DEFAULT_HTTP_TIMEOUT,
+                                      http_headers)
 
 URL = "https://finance.yahoo.com/u/yahoo-finance/watchlists/high-yield-dividend-stocks/"
 
@@ -25,30 +28,34 @@ class HighYield(TypeConverter):
 def get_high_yield_stocks() -> list[HighYield]:
     result: list[HighYield] = []
 
-    web_driver.get(URL)
-    _ = web_driver.find_element(
-        By.CSS_SELECTOR,
-        "#Col1-0-WatchlistDetail-Proxy > div > section:nth-child(5) > div > div > table > tbody"
-    )
-    page_source = web_driver.page_source
-    soup = BeautifulSoup(page_source, "lxml")
-    rs_list = soup.select(
-        "#Col1-0-WatchlistDetail-Proxy > div > section:nth-child(5) > div > div > table > tbody"
-    )
-    for tbl in rs_list:
-        rs_row = tbl.select("tr")
-        for row in rs_row:
-            rs_col = row.select("td")
-            stock = HighYield()
-            stock.symbol = strip(rs_col[0].text)
-            stock.company_name = strip(rs_col[1].text)
-            stock.last_price = to_float(rs_col[2].text)
-            stock.change = to_float(rs_col[3].text)
-            stock.pct_change = to_float(rs_col[4].text)
-            stock.market_time = strip(rs_col[5].text)
-            stock.volume = strip(rs_col[6].text)
-            stock.avg_volume_3_mths = strip(rs_col[7].text)
-            stock.market_cap = strip(rs_col[8].text)
-            result.append(stock)
+    with closing(requests.Session()) as s:
+        s.verify = False
+        s.mount("https://", HTTPAdapter(max_retries=DEFAULT_HTTP_RETRY))
+        r = s.get(
+            URL, verify=True, headers=http_headers(), timeout=DEFAULT_HTTP_TIMEOUT
+        )
+        if r.status_code != requests.status_codes.codes["ok"]:
+            raise ConnectionError(
+                "ERR: error " + str(r.status_code) + ", try again later."
+            )
+        soup = BeautifulSoup(r.text, "lxml")
+        rs_list = soup.select(
+            "#Col1-0-WatchlistDetail-Proxy > div > section:nth-child(5) > div > div > table > tbody"
+        )
+        for tbl in rs_list:
+            rs_row = tbl.select("tr")
+            for row in rs_row:
+                rs_col = row.select("td")
+                stock = HighYield()
+                stock.symbol = strip(rs_col[0].text)
+                stock.company_name = strip(rs_col[1].text)
+                stock.last_price = to_float(rs_col[2].text)
+                stock.change = to_float(rs_col[3].text)
+                stock.pct_change = to_float(rs_col[4].text)
+                stock.market_time = strip(rs_col[5].text)
+                stock.volume = strip(rs_col[6].text)
+                stock.avg_volume_3_mths = strip(rs_col[7].text)
+                stock.market_cap = strip(rs_col[8].text)
+                result.append(stock)
 
     return result
