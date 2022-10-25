a2f3bf8b1f44b37053b83a29fbca374fad482bfc
alpha2phi <alpha2phi@gmail.com>
2022-10-25 19:28:18 +0800

    feat: FundamentalAnalysis

--- alphalib/analysis/fa/__init__.py
+++ alphalib/analysis/fa/__init__.py
@@ -1,15 +1,15 @@
 from dataclasses import dataclass
 
 from alphalib.data_sources.investing import Investing
-from alphalib.data_sources.investing import get_stock_details as get_investing
+from alphalib.data_sources.investing import get_stock_details as from_investing
 from alphalib.data_sources.nasdaq import Nasdaq
-from alphalib.data_sources.nasdaq import get_stock_details as get_nasdaq
+from alphalib.data_sources.nasdaq import get_stock_details as from_nasdaq
 from alphalib.data_sources.seeking_alpha import SeekingAlpha
 from alphalib.data_sources.seeking_alpha import \
-    get_stock_details as get_seeking_alpha
+    get_stock_details as from_seeking_alpha
 from alphalib.data_sources.yahoo_finance import YahooFinance
 from alphalib.data_sources.yahoo_finance import \
-    get_stock_details as get_yfinance
+    get_stock_details as from_yfinance
 
 
 @dataclass
@@ -34,22 +34,22 @@ def all_sources(symbol: str) -> FundamentalAnalysis:
 
 def yahoo_finance(symbol: str) -> YahooFinance:
     assert symbol
-    return get_yfinance(symbol)
+    return from_yfinance(symbol)
 
 
 def seeking_alpha(symbol: str) -> SeekingAlpha:
     assert symbol
-    result = get_seeking_alpha(symbol)
+    result = from_seeking_alpha(symbol)
     if len(result.dividend_history) == 0:
-        return get_seeking_alpha(symbol)
+        return from_seeking_alpha(symbol)
     return result
 
 
 def nasdaq(symbol: str) -> Nasdaq:
     assert symbol
-    return get_nasdaq(symbol)
+    return from_nasdaq(symbol)
 
 
 def investing(symbol: str) -> Investing:
     assert symbol
-    return get_investing(symbol)
+    return from_investing(symbol)
