86b226e8d22faa9bbb034671d80cce5ab678c03c
alpha2phi <alpha2phi@gmail.com>
2023-07-12 21:15:25 +0800

    chore: organize import

--- apps/data_app/alpha/__init__.py
+++ apps/data_app/alpha/__init__.py
@@ -1,7 +1,7 @@
-import numpy as np
 import asyncio
 from datetime import datetime, timezone
 
+import numpy as np
 import pandas as pd
 from streamlit.logger import get_logger
 from yahooquery import Ticker
